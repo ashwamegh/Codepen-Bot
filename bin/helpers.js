ef6ee31a0b83aef897c04b521a854af166209eed
shashank7200 <shashank7200@gmail.com>
2017-05-29 01:54:26 +0530

    initiated handling search command

--- bin/helpers.js
+++ bin/helpers.js
@@ -3,6 +3,8 @@
 const botBuilder = require('claudia-bot-builder');
 const slackTemplate = botBuilder.slackTemplate;
 
+let promise = require('request-promise');
+
 const greetTheUser = (username) => {
   let message = getGreetMessage(username);
     return {
@@ -12,6 +14,62 @@ const greetTheUser = (username) => {
         // "color": "#111"
     };
 };
+
+function setOptions(url) {
+    return {
+            uri: url,
+            headers: { 'User-Agent': 'Request-Promise'},
+            json: true // Automatically parses the JSON string in the response 
+    };
+}
+
+function notFound(){
+    return{
+          "mrkdwn": true,
+          "text": "No results found!"
+      };
+}
+
+const handleSearchCommand = (url, searchTitle) =>{
+  if (url !== ""){
+   return promise(setOptions(url)).then((data) =>{
+      if(data.data!== undefined  ){
+        if(data.data.length > 0){
+
+            let privatResultWithTopic = new slackTemplate(searchTitle);
+
+                data.data.forEach(function(entry, i) {
+
+                i++;
+
+                return privatResultWithTopic.addAttachment('A'+i)
+                    .addTitle(i + ". " + entry.title, entry.link)
+                    .addText(clearResponse(entry.content||entry.details));
+
+            });
+                
+            return privatResultWithTopic.get();   
+        }
+        else{
+              return notFound();
+        }
+   }else{
+     return notFound();
+   } 
+    }).catch(err => console.log(err));
+  }
+  else{
+      return{
+            "mrkdwn": true,
+            "text": "Please enter the category `pens` or `posts` followed by your `Search Query`"
+        };
+  }
+
+}
+
+const clearResponse = (text)=>{
+    return text.replace(/(<([^>]+)>)/ig, "");
+}
 
 const getGreetMessage = (username) => {
     return ">Hello " + username +
@@ -28,5 +86,6 @@ module.exports ={
   BASE_URL : "http://cpv2api.com",
   
   // SLACK CALLBACKS FOR INTEGRATED COMMANDS
-  greetTheUser: greetTheUser
+  greetTheUser: greetTheUser,
+  search      : handleSearchCommand
 }
