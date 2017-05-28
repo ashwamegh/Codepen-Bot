e8a9f989d1c89de0feeb50a989d1c8b4f7e4e5c3
shashank7200 <shashank7200@gmail.com>
2017-05-29 01:53:01 +0530

    fixed decalrations retrieving commands & text with url

--- bin/bot.js
+++ bin/bot.js
@@ -4,14 +4,17 @@ const botBuilder = require('claudia-bot-builder');
 const helpers = require('./helpers');
 
 module.exports = botBuilder(request => {
+  let text = request.text;
   let command = request.originalRequest.command
-  let requestText = request.text.split(" ")
-  let category = requestText.length>1 ? requestText[0] : ""
-  let searchTopics = requestText.length>2 ? requestText.slice(1) : ""
-  let topic = searchTopics.join(" ")
-  let searchTopic = searchTopics.join("&")
-  let url = ""
-  let searchTitle = `>Search results against your query for: ${topic}`
+
+  let requestText = text.split(" ");
+  let category = requestText.length>0 ? requestText[0] : "";
+  let searchTopics = requestText.length>1 ? requestText.slice(1) : "";
+
+  let topic = searchTopics!==""?searchTopics.join(" "): "";
+  let searchTopic = searchTopics!==""?searchTopics.join("&"):"";
+  let url = "";
+  let searchTitle = `>Search results against your query in *${category}* for: *${topic}*`;
       // searchTitle = searchTopics !== "" ? 
 
   switch (command){
@@ -19,8 +22,9 @@ module.exports = botBuilder(request => {
       return helpers.greetTheUser(request.originalRequest.user_name);
     
     case helpers.SEARCH_COMMAND:
-      url = category !== ""?helpers.BASE_URL+`/${category}/?q=${searchTopic}`: helpers.BASE_URL
+      url = category !== ""?helpers.BASE_URL+`/search/${category}/?q=${searchTopic}`:"";
       return helpers.search(url, searchTitle)
+
     default:
       console.log(`Oops! I think you forgot something. Please recheck your command ${command}`);
 
